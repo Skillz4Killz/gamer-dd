354bff0b973fa8dfec8fbb7101fe3140f73a5890
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-06-08 19:14:16 +0000

    fix: disable prefix command for now

--- src/core/commands/Prefix.ts
+++ src/core/commands/Prefix.ts
@@ -1,8 +1,8 @@
 import type { Context } from "oasis-framework";
 import type { BotWithCache } from "discordeno/cache-plugin";
 import { Argument, Command } from "oasis-framework";
-import { hasGuildPermissions } from "discordeno/permissions-plugin";
-import Prisma from "../../internals/database.js";
+// import { hasGuildPermissions } from "discordeno/permissions-plugin";
+// import Prisma from "../../internals/database.js";
 
 @Command
 export class Prefix {
@@ -21,72 +21,75 @@ export class Prefix {
     }
 
     async run(ctx: Context<BotWithCache>) {
-        const prefix = ctx.options.getString(0) ?? ctx.options.getString("prefix");
+        // const prefix = ctx.options.getString(0) ?? ctx.options.getString("prefix");
 
         if (!ctx.guildId) {
             await ctx.respondPrivately({ with: "You must be in a guild to use this command" });
             return;
         }
 
-        const currentGuild = await Prisma.guild.findUnique({
-            where: {
-                id: ctx.guildId,
-            },
-        });
+        // const currentGuild = await Prisma.guild.findUnique({
+        //     where: {
+        //         id: ctx.guildId,
+        //     },
+        //     select: {
+        //         prefix: true,
+        //     },
+        // });
 
-        if (!prefix && currentGuild != null) {
-            await ctx.respond({ with: `The current prefix is \`${currentGuild.prefix}\`` });
-            return;
-        }
+        // if (!prefix && currentGuild != null) {
+        //     await ctx.respond({ with: `The current prefix is \`${currentGuild.prefix}\`` });
+        //     return;
+        // }
 
-        if (!prefix && currentGuild == null) {
-            await ctx.respondPrivately({ with: "You must provide a prefix" });
-            return;
-        }
+        // if (!prefix && currentGuild == null) {
+        //     await ctx.respondPrivately({ with: "You must provide a prefix" });
+        //     return;
+        // }
 
-        if (!prefix) {
-            return;
-        }
+        // if (!prefix) {
+        //     return;
+        // }
 
-        if (prefix.length > 10) {
-            await ctx.respondPrivately({ with: "Prefixes cannot be longer than 10 characters" });
-            return;
-        }
+        // if (prefix.length > 10) {
+        //     await ctx.respondPrivately({ with: "Prefixes cannot be longer than 10 characters" });
+        //     return;
+        // }
 
-        if (!ctx.userId) {
-            return;
-        }
+        // if (!ctx.userId) {
+        //     return;
+        // }
 
-        // check user permissions
-        const member =
-            ctx.bot.members.get(BigInt(`${ctx.guildId}${ctx.userId}`)) ??
-            (await ctx.bot.helpers.getMember(ctx.guildId, ctx.userId));
+        // // check user permissions
+        // const member =
+        //     ctx.bot.members.get(BigInt(`${ctx.guildId}${ctx.userId}`)) ??
+        //     (await ctx.bot.helpers.getMember(ctx.guildId, ctx.userId));
 
-        if (!member) {
-            return;
-        }
+        // if (!member) {
+        //     return;
+        // }
 
-        if (!hasGuildPermissions(ctx.bot, ctx.guildId, member, ["MANAGE_GUILD"])) {
-            await ctx.respondPrivately({
-                with: "You must have the `MANAGE_GUILD` permission to use this command",
-            });
-            return;
-        }
+        // if (!hasGuildPermissions(ctx.bot, ctx.guildId, member, ["MANAGE_GUILD"])) {
+        //     await ctx.respondPrivately({
+        //         with: "You must have the `MANAGE_GUILD` permission to use this command",
+        //     });
+        //     return;
+        // }
 
-        // upsert prefix
-        const guild = await Prisma.guild.upsert({
-            where: {
-                id: ctx.guildId,
-            },
-            update: {
-                prefix,
-            },
-            create: {
-                id: ctx.guildId,
-                prefix,
-            },
-        });
+        // // upsert prefix
+        // const guild = await Prisma.guild.upsert({
+        //     where: {
+        //         id: ctx.guildId,
+        //     },
+        //     update: {
+        //         prefix,
+        //     },
+        //     create: {
+        //         id: ctx.guildId,
+        //         prefix,
+        //     },
+        // });
 
-        await ctx.respond({ with: `Prefix set to \`${guild?.prefix}\`` });
+        // await ctx.respond({ with: `Prefix set to \`${guild?.prefix}\`` });
     }
 }
